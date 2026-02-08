4697814174b474529dace403ccf3dd432bc523a6
João Thallis <joaothallis.developer@gmail.com>
2026-02-08 13:58:35 -0300

    Add i3

--- configuration.nix
+++ configuration.nix
@@ -53,6 +53,7 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.windowManager.i3.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
