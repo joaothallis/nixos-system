01c7094e8986a531048c175b869b1af1d44916bc
João Thallis <joaothallis.developer@gmail.com>
2026-02-21 12:57:24 -0300

    Setup bash

--- home.nix
+++ home.nix
@@ -6,6 +6,22 @@
   home.username = "joao";
   home.homeDirectory = "/home/joao";
 
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    nix-direnv.enable = true;
+  };
+  programs.bash.enable = true;
+  programs.bash.shellAliases = {
+    gs = "git status";
+    gd = "git diff";
+    gl = "git pull --prune";
+    gp = "git push";
+    glog = "git log --oneline";
+    gc = "git commit --patch";
+    gca = "git commit --patch --amend";
+  };
+
   programs.chromium.enable = true;
 
   # This value determines the Home Manager release that your
