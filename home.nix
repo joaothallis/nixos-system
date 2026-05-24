81b880eadfe8fb4f8457c779f18ed142b0686405
João Thallis <joaothallis.developer@gmail.com>
2026-05-24 12:59:50 -0300

    Bump home.stateVersion to 26.05

--- home.nix
+++ home.nix
@@ -230,7 +230,7 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "25.11";
+  home.stateVersion = "26.05";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
