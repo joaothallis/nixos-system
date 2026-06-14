09cfee51cc29bd04b82762610484806f8fe1a701
João Thallis <joaothallis.developer@gmail.com>
2026-06-14 12:28:41 -0300

    Add gemini-cli

--- configuration.nix
+++ configuration.nix
@@ -166,6 +166,7 @@ in
     pkgs.nixfmt
     inputs.codex-cli-nix.packages.${pkgs.stdenv.hostPlatform.system}.default
     claude-code
+    gemini-cli
     pkgs.mcp-nixos
     pkgs.jellyfin-desktop
   ];
